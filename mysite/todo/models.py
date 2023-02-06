4c1f3d8c1cb5eb4c2f74e99e1079b3779f4a9672
vkfkd1107 <ji_su1107@naver.com>
2023-02-06 20:51:33 +0900

    feat(todo model): create Todo model

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)

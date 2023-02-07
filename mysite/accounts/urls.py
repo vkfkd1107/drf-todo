cce4dafc87d7dfe70adfeb15c5452dd43a905767
vkfkd1107 <ji_su1107@naver.com>
2023-02-07 19:56:19 +0900

    feat(jwt): create token_obtain_pair, token_refresh

---
+++ mysite/accounts/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from todo import views
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
